a7f00596348906665e2fb8cb1a5953ff95f9589d
Hans Martin Johansen <hmtj@mailo.com>
2021-09-26 22:14:44 +0200

    forced user to enter password

--- archinstall-inchroot.sh
+++ archinstall-inchroot.sh
@@ -21,3 +21,7 @@ grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 # Set grub locale and make config
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Make password
+echo "enter password for root"
+passwd
