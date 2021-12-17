746848a95c8b6cb45ae5f9d1c120856bea7843c4
Hans Martin Johansen <hmtj@mailo.com>
2021-12-17 12:58:48 +0100

    added support for btrfs

--- archinstall.sh
+++ archinstall.sh
@@ -11,6 +11,7 @@ Server = http://mirror.terrahost.no/linux/archlinux/$repo/os/$arch
 '
 
 PARTITION_DISK=''
+BTRFS=true
 [ -z $PARTITION_DISK ] && echo 'partitioned disk has not been selected, please edit this in file' && exit 1
 
 
@@ -21,16 +22,31 @@ read -p 'Setup done, press enter to continue'
 
 
 # FORMATING PARTITIONS WITH FILE SYSTEMS
+# Mounting /EFI happens in chroot
 mkfs.fat -F32 "${PARTITION_DISK}p1"
 mkswap "${PARTITION_DISK}p2"
-mkfs.ext4 "${PARTITION_DISK}p3"
-mkfs.ext4 "${PARTITION_DISK}p4"
-read -p 'Made filesystem, press enter to continue' 
-
-# MOUNTING and SWAPON
 swapon "${PARTITION_DISK}p2"
-mount "${PARTITION_DISK}p3" /mnt
-mkdir /mnt/home && mount "${PARTITION_DISK}p4" /mnt/home
+
+if $BTRFS; then
+    mkfs.btrfs "${PARTITION_DISK}p3"
+    mount "${PARTITION_DISK}p3" /mnt
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@home
+    btrfs subvolume create /mnt/@snapshots
+    umount /mnt
+    mount -o noatime,compress=no,space_cache=v2,subvol=@ \
+        "${PARTITION_DISK}p3" /mnt
+    mkdir -p /mnt/{boot,home,.snapshots}
+    # Same options implicitly apply to these mounts
+    mount -o subvol=@home "${PARTITION_DISK}p3" /mnt/home
+    mount -o subvol=@snapshots "${PARTITION_DISK}p3" /mnt/.snapshots
+else
+    mkfs.ext4 "${PARTITION_DISK}p3"
+    mkfs.ext4 "${PARTITION_DISK}p4"
+    mount "${PARTITION_DISK}p3" /mnt
+    mkdir /mnt/home && mount "${PARTITION_DISK}p4" /mnt/home
+fi
+
 read -p 'Mounted partitions, press enter to continue'
 
 # INSTALLING BASE
@@ -38,17 +54,18 @@ pacstrap -i /mnt base base-devel
 # GENERATE file system tab
 genfstab -U -p /mnt >> /mnt/etc/fstab
 # Moving into install
-
 arch-chroot /mnt << EOF
-[ -z $BOOT_PARTITION ] && echo "something went wrong" && exit 1
-
 # Timezone
 ln -sf /usr/share/zoneinfo/Europe/Oslo /etc/localtime
 # Harwareclock
 hwclock --systohc
 
 # Install needed packages, like linux and bootloader
-pacman -S --noconfirm grub efibootmgr dosfstools openssh os-prober mtools linux-headers linux-zen linux-zen-headers networkmanager neovim linux-firmware
+pacman -S --noconfirm\
+    grub efibootmgr dosfstools openssh os-prober mtools \
+    linux-zen linux-zen-headers base-devel \
+    networkmanager neovim linux-firmware git \
+    xdg-utils xdg-user-dils
 
 # Set locale (by uncommenting the english)
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
@@ -56,7 +73,11 @@ locale-gen
 
 # Make boot directory and mount boot partition
 mkdir /boot/EFI
-mount "$BOOT_PARTITION" /boot/EFI
+mount "${BOOT_PARTITION}p1" /boot/EFI
+
+if [ "$BTRFS" ]; then
+    sed -i 's/MODULES=()/MODULES=(btrfs)/g' /etc/mkinitcpio.conf
+fi
 
 # Install grub on EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
